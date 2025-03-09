a778f5c0d262110f5b4261619f37eae7cc307d53
AITBOUKHA Elias <elias.aitboukha@assurance-maladie.fr>
2025-03-10 00:09:27 +0100

    inscription

--- front/src/auth/Inscription.tsx
+++ front/src/auth/Inscription.tsx
@@ -1,4 +1,25 @@
 import { NavLink } from "react-router-dom";
+function handleInscription(event: React.FormEvent<HTMLFormElement>) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  const formValues = Object.fromEntries(formData.entries());
+
+  console.log("Form values:", formValues);
+
+  fetch("http://localhost:3001/inscription", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formValues),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Server response:", data);
+    });
+
+}
+
 
 function Inscription() {
   return (
@@ -6,7 +27,7 @@ function Inscription() {
       <section className="text-gray-600 body-font flex flex-col justify-center items-center">
         <h1 className="text-3xl font-semibold mt-5">Inscription</h1>
         <div className="container px-5 py-6 mx-auto flex flex-wrap items-center justify-center">
-          <div className="lg:w-2/5 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col w-full mt-0 justify-center">
+          <form onSubmit={handleInscription} className="lg:w-2/5 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col w-full mt-0 justify-center">
             <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
               Créer un compte
             </h2>
@@ -31,12 +52,13 @@ function Inscription() {
               </div>
             </div>
 
+
             <div className="relative mb-4 flex justify-center">
               <div className="h-32 w-32 bg-indigo-500 overflow-hidden rounded-full">
                 <input
                   type="file"
-                  id="profile-picture"
-                  name="profile-picture"
+                  id="photo"
+                  name="photo"
                   className="opacity-0 h-full w-full"
                 />
                 <ion-icon
@@ -45,59 +67,46 @@ function Inscription() {
                 ></ion-icon>
               </div>
             </div>
+            <p className="text-xs text-muted-foreground p-2 m-4">Les champs marqués d'un * sont obligatoires</p>
             <div className="relative mb-4">
               <label
-                htmlFor="full-name"
+                htmlFor="pseudo"
                 className="leading-7 text-sm text-gray-600"
               >
                 Pseudo
               </label>
               <input
                 type="text"
-                id="full-name"
-                name="full-name"
+                id="pseudo"
+                name="pseudo"
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
             <div className="relative mb-4">
               <label
-                htmlFor="full-name"
+                htmlFor="tag"
                 className="leading-7 text-sm text-gray-600"
               >
                 Tag
               </label>
               <input
                 type="text"
-                id="full-name"
-                name="full-name"
+                id="tag"
+                name="tag"
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
             <div className="relative mb-4">
               <label
-                htmlFor="full-name"
-                className="leading-7 text-sm text-gray-600"
-              >
-                Email
-              </label>
-              <input
-                type="text"
-                id="full-name"
-                name="full-name"
-                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div className="relative mb-4">
-              <label
-                htmlFor="full-name"
+                htmlFor="tel"
                 className="leading-7 text-sm text-gray-600"
               >
                 Tel.
               </label>
               <input
                 type="tel"
-                id="full-name"
-                name="full-name"
+                id="tel"
+                name="tel"
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
@@ -117,7 +126,7 @@ function Inscription() {
             </div>
             <div className="relative mb-4">
               <label
-                htmlFor="email"
+                htmlFor="adresse"
                 className="leading-7 text-sm text-gray-600"
               >
                 Adresse
@@ -127,12 +136,19 @@ function Inscription() {
                 id="adresse"
                 name="adresse"
                 onChange={(e) => {
-                  console.log(e.target.value);
+                  if (e.target.value.length > 3) {
                   fetch("https://api-adresse.data.gouv.fr/search/?q=" + encodeURIComponent(e.target.value)+ "&limit=1&autocomplete=1")
                     .then(response => response.json())
                     .then(data => {
+                      if (data.features.length === 0) {
+                        document.getElementById("adresseautocomplete").innerHTML = "<p class='my-4'>Aucun résultat</p>";
+                        return;
+                      }
+                      else {
                       document.getElementById("adresseautocomplete").innerHTML = data.features.map(feature => `<p class="my-4">${feature.properties.label}</p>`).join('<hr class="border-t mx-5 border-3 border-indigo-500 my-4"/>');
+                      }
                     });
+                  }
                 }
                 }
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
@@ -140,11 +156,11 @@ function Inscription() {
             
             </div>
             <div className="mb-4">
-              <div id="adresseautocomplete" className="text-xl font-semibold w-full p-3 bg-indigo-200 text-center">...</div>
+              <div id="adresseautocomplete" className="font-semibold w-full p-2 bg-indigo-200 text-center">...</div>
             </div>
             <div className="relative mb-4">
               <label
-                htmlFor="email"
+                htmlFor="bio"
                 className="leading-7 text-sm text-gray-600"
               >
                 Bio
@@ -158,34 +174,36 @@ function Inscription() {
             </div>
             <div className="relative mb-4">
               <label
-                htmlFor="email"
+                htmlFor="motdepasse"
                 className="leading-7 text-sm text-gray-600"
               >
                 Mot de passe
               </label>
               <input
                 type="password"
-                id="email"
-                name="email"
+                id="motdepasse"
+                name="motdepasse"
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
-            <button  className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            <button type="submit"  className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
               Inscription
             </button>
             <div className="flex my-4">
-              <button className="mr-2 flex-1/2 text-white bg-indigo-400 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-500 rounded text-lg">
+              <NavLink to="/connexion" className="mr-2 flex-2/3 text-white bg-indigo-400 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-500 rounded text-lg">
                 Connexion
-              </button>
-              <button className="ml-2 flex-1/2 .text-gray-700 bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-300 rounded text-lg">
-                Retour
-              </button>
+              </NavLink>
+              <NavLink to="/" className="ml-2 flex-1/3 .text-gray-700 bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-300 rounded text-lg">
+                <span className="h-full flex items-center text-2xl">
+                  <ion-icon name="arrow-back-outline"></ion-icon>
+                </span>
+              </NavLink>
             </div>
             <p className="text-xs text-gray-500 mt-3">
               Lors de l'inscription, vous acceptez nos Conditions d'utilisation
               et notre Politique de confidentialité
             </p>
-          </div>
+          </form>
         </div>
       </section>
     </>
