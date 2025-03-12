6de17650d5f00bb402827f86a5d0fea1508f49b0
François <totofrancois12@gmail.com>
2025-03-12 01:32:46 +0100

    refacto an some improuv

--- voisin-api/src/app.module.ts
+++ voisin-api/src/app.module.ts
@@ -2,35 +2,47 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { getMongoConfig } from './config/mongo.config';
-import neo4jConfig from './config/neo4j.config';
 import { TypeOrmConfigService } from './config/posgres.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Neo4jModule } from './config/neo4j.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AuthService } from './auth/auth.service';
+import { EmailModule } from './email/email.module';
+import { NotificationsService } from './user/notifications/notifications.service';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { MessageModule } from './message/message.module';
+import { Neo4jModule } from 'nest-neo4j'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
-      load: [neo4jConfig],
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: getMongoConfig,
       inject: [ConfigService],
     }),
+    Neo4jModule.forRoot({
+      scheme: 'neo4j',
+      host: 'localhost',
+      port: 7687,
+      username: 'neo4j',
+      password: 'password'
+    }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useClass: TypeOrmConfigService,
     }),
+    EventEmitterModule.forRoot(),
     Neo4jModule,
     UserModule,
-    AuthModule
+    EmailModule,
+    AuthModule,
+    MessageModule
   ],
   controllers: [],
-  providers: [AuthService],
+  providers: [AuthService,NotificationsService],
 })
 export class AppModule {}
