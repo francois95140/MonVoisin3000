7c8a92698f9e59557842ecfb686298206a9add11
AITBOUKHA Elias <elias.aitboukha@gmail.com>
2025-07-20 13:39:17 +0200

    fix home navlinks

--- front/src/home/Home.tsx
+++ front/src/home/Home.tsx
@@ -34,14 +34,14 @@ const HomePage = () => {
 
         {/* <!-- Boutons d'action --> */}
         <div style={{animationDelay: '0.2s'}} className="space-y-4 mb-12 fade-in">
-            <a href="./inscription.html" className="btn-primary w-full py-4 rounded-2xl text-white font-semibold text-lg shadow-lg flex items-center justify-center space-x-2">
+            <NavLink to="/inscription" className="btn-primary w-full py-4 rounded-2xl text-white font-semibold text-lg shadow-lg flex items-center justify-center space-x-2">
                 <ion-icon name="rocket" className="text-xl"></ion-icon>
                 <span>Commencer l'aventure</span>
-            </a>
-            <a href="./login.html" className="btn-secondary w-full py-4 rounded-2xl text-white font-medium flex items-center justify-center space-x-2">
+            </NavLink>
+            <NavLink to="/connexion" className="btn-secondary w-full py-4 rounded-2xl text-white font-medium flex items-center justify-center space-x-2">
                 <ion-icon name="phone-portrait" className="text-xl"></ion-icon>
                 <span>J'ai déjà un compte</span>
-            </a>
+            </NavLink>
         </div>
 
         {/* <!-- Statistiques --> */}
